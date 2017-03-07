eaecab965d3cdde64c413a4f4ff16bc34b6f5fee
Matthew Frampton <mattcmf@gmail.com>
2017-03-07 19:27:01 +0000

    Base tests

--- app/src/androidTest/java/demos/udemybraintrainer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/demos/udemybraintrainer/ExampleInstrumentedTest.java
@@ -9,11 +9,6 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -22,5 +17,104 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("demos.udemybraintrainer", appContext.getPackageName());
+    }
+
+    /*
+    Tip: use tags to find out which button has been pressed
+            (Think about using a way to re-use formatting)
+
+    */
+
+    @Test
+    public void GivenTheSplashScreenIsLoaded() {
+        //DisplayGoButton
+
+        //GoButtonIsGreen
+
+        //OnClickDisappears
+
+        //OnClickShowMainMenu
+    }
+
+    @Test
+    public void WhenTheGamePageIsDisplayed(){
+
+        //Display a 4 grid in the middle of the screen
+
+        //TopLeft is pink
+        //TopRight is blue
+        //BottomLeft is red
+        //BottomRight is green
+
+        //All Squares have no values
+    }
+
+    @Test
+    public void Timer(){
+        //Timer is displayed on the top left
+        //Given Go has been clicked
+        //      Then set Timer to 30
+        //Timer decrements at 1 second
+        //Timer can display 0
+        //Timer colour is yellow
+        //Timer is prefixed by s
+    }
+
+    @Test
+    public void QuestionBox(){
+        //Given Go has been clicked
+        // Two numbers between 1 and 40 displayed
+        // Format is "X + Y"
+        //Background is transparent
+        //Alignment is top center
+    }
+
+    @Test
+    public void ScoreDisplay() {
+        //Box colour?
+        //Alignment is top Right
+    }
+
+    @Test
+    public void ScoreCorrect(){
+        //Given a fresh load Display 0/0
+        //When I answer a question correct Then
+        //Then Increase both score and question count
+        //And display new question
+        //And display new answers
+    }
+
+    @Test
+    public void ScoreIncorrect(){
+        //Given a fresh load Display 0/0
+        //When I answer a question correct Then
+        //Then Increase ONLY question count
+        //And display new question
+        //And display new answers
+    }
+
+    @Test
+    public void GameMessageCorrect(){
+        //Given I have answered a question right
+        //Then display "Correct"
+        //And layout is bottom middle
+    }
+
+
+    @Test
+    public void GameMessageTimerEnd(){
+        //Given the timer is 0
+        //And alignment is bottom middle
+        //Then "Your score: 5/16"
+    }
+
+    @Test
+    public void PlayAgainButton(){
+        //Given the timer is at 0
+        // When the play again button has been clicked
+        //Then Reset score
+        //And Reset question
+        //And reset GameMessage output
+        //And alignment is bottom middle below game message
     }
 }
